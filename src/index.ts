b19558d7b40bb79a745587cb1d18e1fa5b79f565
hillac <lachiehill@gmail.com>
2024-03-22 16:33:43 +1000

    new env init in auth core

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { FastifyAuth, getSession } from "@auth/fastify"
-import Fastify from 'fastify';
-import formbodyParser from "@fastify/formbody"
-import GoogleProvider from "@auth/core/providers/google";
+import { FastifyAuth, getSession } from "@auth/fastify";
+import Fastify from "fastify";
+import formbodyParser from "@fastify/formbody";
+import GoogleProvider from "@auth/fastify/providers/google";
 import dotenv from "dotenv";
 
 dotenv.config({ path: `.env.local` });
@@ -9,45 +9,39 @@ dotenv.config({ path: `.env.local` });
 const fastify = Fastify({ logger: true });
 
 const config = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-    ],
-    secret: process.env.AUTH_SECRET!,
-    trustHost: true,
+  providers: [GoogleProvider],
 };
 
 const AuthPlugin = FastifyAuth(config);
 fastify.register(formbodyParser);
-fastify.register(
-    AuthPlugin,
-    { prefix: "/api/auth" },
-)
+fastify.register(AuthPlugin, { prefix: "/api/auth" });
 
-fastify.decorate('authenticate', async (req, reply) => {
-        const session = await getSession(req, config);
-        if (!session) {
-            reply.status(403).send("Unauthorized");
-            return;
-        }
-        reply.user = session?.user; // Decorating the reply object
+fastify.decorate("authenticate", async (req, reply) => {
+  const session = await getSession(req, config);
+  if (!session) {
+    reply.status(403).send("Unauthorized");
+    return;
+  }
+  reply.user = session?.user; // Decorating the reply object
 });
 
-fastify.get('/api/users/me', { preHandler: [fastify.authenticate] }, async (req, reply) => {
-    reply.type('text/html').send(
-    `
+fastify.get(
+  "/api/users/me",
+  { preHandler: [fastify.authenticate] },
+  async (req, reply) => {
+    reply.type("text/html").send(
+      `
         <h1>Profile</h1>
-        <pre>${JSON.stringify(reply.user?.name, null, 2)}</pre>
+        <pre>${JSON.stringify(reply.user, null, 2)}</pre>
         <a href="/">Home</a>
     `
     );
-});
+  }
+);
 
 fastify.get("/", (req, reply) => {
-    reply.type('text/html').send(
-        `
+  reply.type("text/html").send(
+    `
             <h1>Welcome to Auth.js + Fastify Demo!</h1>
             <ol>
             <li>Sign in at <a href="/api/auth/signin">/api/auth/signin</a> </li>
@@ -55,12 +49,12 @@ fastify.get("/", (req, reply) => {
             <li>Access the current user at <a href="/api/users/me">/api/users/me</a> </li>
             </ol>
         `
-    );
+  );
 });
 
 fastify.listen({ port: 3000 }, function (err, address) {
-    if (err) {
-      fastify.log.error(err)
-      process.exit(1)
-    }
-})
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+});
